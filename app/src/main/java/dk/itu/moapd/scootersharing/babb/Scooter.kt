7960a68a8c9843ccf0aae89a024ac068f3e91382
Babette Bækgaard <38117208+BabetteB@users.noreply.github.com>
2023-01-31 11:14:41 +0100

    first commit - compile errors - cuz im doin shit n stuff u know

---
+++ app/src/main/java/dk/itu/moapd/scootersharing/babb/Scooter.kt
@@ -0,0 +1,23 @@
+package dk.itu.moapd.scootersharing.babb
+
+class Scooter {
+
+    private var name: String
+    private var location: String
+
+    constructor(name: String, location: String){
+        this.name = name
+        this.location = location
+    }
+
+    fun getName(): String {
+        return name
+    }
+
+    fun setName(name: String){
+        this.name = name
+    }
+
+    fun
+
+}
